860a047dd1f96829036f208880093ca75bb91916
Orlin89 <149891759+Orlin89@users.noreply.github.com>
2025-05-01 20:09:35 +0300

    Set browser headless for CI

--- SauceDemoAutomationUI/Drivers/WebDriverFactory.cs
+++ SauceDemoAutomationUI/Drivers/WebDriverFactory.cs
@@ -4,6 +4,7 @@ using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Edge;
 using WebDriverManager;
 using WebDriverManager.DriverConfigs.Impl;
+using System;
 
 namespace SauceDemoAutomationUI.Drivers
 {
@@ -13,21 +14,42 @@ namespace SauceDemoAutomationUI.Drivers
         {
             IWebDriver driver;
 
+            bool isCi = Environment.GetEnvironmentVariable("CI") == "true";
+
             switch (browser.ToLower())
             {
                 case "chrome":
                     new DriverManager().SetUpDriver(new ChromeConfig());
-                    driver = new ChromeDriver();
+                    var chromeOptions = new ChromeOptions();
+                    if (isCi)
+                    {
+                        chromeOptions.AddArgument("--headless=new");
+                        chromeOptions.AddArgument("--no-sandbox");
+                        chromeOptions.AddArgument("--disable-dev-shm-usage");
+                        chromeOptions.AddArgument("--disable-gpu");
+                    }
+                    driver = new ChromeDriver(chromeOptions);
                     break;
 
                 case "firefox":
                     new DriverManager().SetUpDriver(new FirefoxConfig());
-                    driver = new FirefoxDriver();
+                    var firefoxOptions = new FirefoxOptions();
+                    if (isCi)
+                    {
+                        firefoxOptions.AddArgument("--headless");
+                    }
+                    driver = new FirefoxDriver(firefoxOptions);
                     break;
 
                 case "edge":
                     new DriverManager().SetUpDriver(new EdgeConfig());
-                    driver = new EdgeDriver();
+                    var edgeOptions = new EdgeOptions();
+                    if (isCi)
+                    {
+                        edgeOptions.AddArgument("headless");
+                        edgeOptions.AddArgument("disable-gpu");
+                    }
+                    driver = new EdgeDriver(edgeOptions);
                     break;
 
                 default:
